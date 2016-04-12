fcd0efbeb6e6559304bac929a9b97f4a9b06ade4
Fahad Murtaza <fahdim@gmail.com>
2016-04-13 01:20:08 +0500

    Input in python

---
+++ 004-input.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+value = raw_input("\n\nPress the enter key to exit.")
+print value
---
+++ 005-raw-to-int.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+intvalue = int(raw_input("\n\nPress the enter key to exit."))
+print intvalue
